90c972fc9b53f45b2fd22d793eba9a8931b2a6a6
tomasarizu <tarizu@udesa.edu.ar>
2022-06-10 14:52:50 -0300

    ya valide el forms del header

--- js/playlist.js
+++ js/playlist.js
@@ -1,1 +1,16 @@
+let navegador = document.getElementById('navegador');
+
+let form = document.querySelector('.form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if (navegador.value == "" || navegador.value.length <= 3) {
+        alert("Esta vacio el campo de busqueda o debe ser mayor a 3 caracteres");
+    } else {
+        this.submit();
+    }
+
+})
+
 const url = 'https://developers.deezer.com/api/chart ';
