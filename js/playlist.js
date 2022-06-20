71afe4e54cdfb61933ce0fe92d5729d4316d9bd0
GITHUBZac <zofarrell@udesa.edu.ar>
2022-06-20 17:50:49 -0300

    cosas

--- js/playlist.js
+++ js/playlist.js
@@ -1,6 +1,6 @@
 let navegador = document.getElementById('navegador');
 
-/*
+
 let form = document.querySelector('.form');
 
 form.addEventListener('submit', function(e) {
@@ -13,9 +13,9 @@ form.addEventListener('submit', function(e) {
     }
     
 })
-*/
 
-const url = 'https://api.allorigins.win/raw?url=https://developers.deezer.com/api/chart ';
+
+
 
 let fav_arr = JSON.parse(localStorage.getItem('favs'));
 //let favoritos = JSON.parse(recuperoStorage);
@@ -37,10 +37,12 @@ if (fav_arr == null || fav_arr.length == 0) {
         /* Buscar el personaje */
         let id = fav_arr[i];
         const URL = 'https://api.allorigins.win/raw?url=https://api.deezer.com/track/' + id;
-    
 
-            
-            then(function(data){
+        fetch(URL)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data){
                 personajesFavoritos += `<a href="/detailsong.html?id=${id}">
                                             <article>
                                                 <img src=${data.album.cover}>
@@ -48,4 +50,51 @@ if (fav_arr == null || fav_arr.length == 0) {
                                                 
                                                 </article> 
                                                 <hr></a>` 
-                        
+                                                
+                section.innerHTML = personajesFavoritos;
+            }).catch(function (error) {
+                console.log(error);
+            })
+    }
+}
+let albumFav = JSON.parse(localStorage.getItem('albumFav'));
+console.log(albumFav);
+//let favoritos = JSON.parse(recuperoStorage);
+
+/* capturar el elemento en el dom */;
+
+let section2 = document.querySelector('.albumArticles');
+
+let personajesFavoritos2 = '';
+
+/* Evaluar el localStorage */
+
+if (albumFav == null || albumFav.length == 0) {
+    section.innerHTML = '<p>No hay items en Favoritos</p>';
+} else {
+    /* Si contiene elementos */
+
+    for (let i = 0; i < albumFav.length; i++) {
+        /* Buscar el personaje */
+        let id2 = albumFav[i];
+        const URL2 = 'https://api.allorigins.win/raw?url=https://api.deezer.com/album/' + id2;
+
+        fetch(URL2)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data){
+                personajesFavoritos2 += `<a href="/detail-album.html?id=${id2}">
+                                            <article>
+                                                <img src=${data.cover_medium}>
+                                                <p>Nombre: ${data.title}</p>
+                                                
+                                                </article> 
+                                                <hr></a>` 
+                                                
+                section2.innerHTML = personajesFavoritos2;
+            }).catch(function (error) {
+                console.log(error);
+            })
+    }
+}
